49355cd536c34a54e7dc38142de0edbc17a1e0cc
Gabriel Augusto <decauser@hotmail.com>
2022-12-28 10:30:36 -0300

    1py

---
+++ Lista 1 - programação/Q1.py
@@ -0,0 +1,7 @@
+#1) Receber três números e imprimir o maior.
+
+n1 = int(input("Digite o primeiro numero "))
+n2 = int(input("Digite o segundo numero "))
+n3 = int(input("Digite o terceiro numero "))
+
+print("O maior valor e: "+str(max(n1, n2, n3)))
